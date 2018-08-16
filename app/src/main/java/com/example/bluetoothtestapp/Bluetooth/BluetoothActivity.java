9e1e3a13f7113e78d52adc02e148f305d2cef9c9
tikepjt <Tikepjt@gmail.com>
2018-08-16 18:06:39 +0900

    first commit

---
+++ app/src/main/java/com/example/bluetoothtestapp/Bluetooth/BluetoothActivity.java
@@ -0,0 +1,19 @@
+package com.example.bluetoothtestapp.Bluetooth;
+
+import android.os.Bundle;
+
+import com.example.bluetoothtestapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BluetoothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+
+
+    }
+
+}
